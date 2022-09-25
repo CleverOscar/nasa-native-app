4995a9b5516fe7d6a8bc48cb0115a30ca3329921
cleveroscar <Oscarpa9188@gmail.com>
2022-09-25 11:58:04 -0500

    Home Screen
    Home Screen created
    
    Home Screen component showing

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import HomeScreen from './Views/HomeScreen';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Nasa Native Application</Text>
-      <StatusBar style="auto" />
+      <HomeScreen />
     </View>
   );
 }
